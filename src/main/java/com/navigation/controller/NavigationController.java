741c9d11f249695cd2d9de9985943118d2f3af8e
Napat <napat.tan@nxz-group.com>
2025-03-08 22:48:47 +0700

    feat: add controller for rover navigation to use with API call

---
+++ src/main/java/com/navigation/controller/NavigationController.java
@@ -0,0 +1,39 @@
+package com.navigation.controller;
+
+import com.navigation.model.NavigateRequestBody;
+import com.navigation.model.Node;
+import com.navigation.model.Output;
+import com.navigation.service.NavigationService;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NavigationController {
+
+  private final NavigationService navigationService;
+
+  @Autowired
+  public NavigationController(NavigationService navigationService) {
+    this.navigationService = navigationService;
+  }
+
+  @GetMapping("rover/navigate")
+  public ResponseEntity<Output> navigateRover(@RequestBody NavigateRequestBody requestBody) {
+    List<Node> obstacles = getObstacleNodes(requestBody);
+    Output output =
+        navigationService.navigateRover(
+            requestBody.getGridSize(), obstacles, requestBody.getCommands());
+    return ResponseEntity.ok(output);
+  }
+
+  public List<Node> getObstacleNodes(NavigateRequestBody requestBody) {
+    return Arrays.stream(requestBody.getObstacles())
+        .map(NavigateRequestBody::convertTupleToNode)
+        .toList();
+  }
+}
--- src/main/java/com/navigation/model/NavigateRequestBody.java
+++ src/main/java/com/navigation/model/NavigateRequestBody.java
@@ -3,18 +3,20 @@ package com.navigation.model;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
+import lombok.Data;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder
+@Data
 public class NavigateRequestBody {
   private Integer gridSize;
   private String[] obstacles;
   private String commands;
 
-  private Node convertTupleToNode(String obstacleLocation) {
+  public static Node convertTupleToNode(String obstacleLocation) {
     String trimmed = obstacleLocation.replace(")", "").replace("(", "");
-    Integer verticalLocation = Integer.valueOf(trimmed.split(",")[0]);
-    Integer horizontalLocation = Integer.valueOf(trimmed.split(",")[1]);
+    Integer horizontalLocation = Integer.valueOf(trimmed.split(",")[0]);
+    Integer verticalLocation = Integer.valueOf(trimmed.split(",")[1]);
     return Node.builder()
         .verticalPosition(verticalLocation)
         .horizontalPosition(horizontalLocation)
